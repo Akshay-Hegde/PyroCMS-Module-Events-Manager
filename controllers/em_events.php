cae62c5e9e23c988e5683e50709ed0984f74e174
Phil Martinez <phil@philsquare.com>
2013-09-12 15:40:33 -0500

    added rough calendar view

--- controllers/em_events.php
+++ controllers/em_events.php
@@ -28,8 +28,8 @@ class Em_events extends Public_Controller
 		// Templates use this lib
 		$this->load->library('table');
 		
-		// Set CP GUI table attr
-		$this->table->set_template(array('table_open'  => '<table class="table-list" border="0" cellspacing="0">'));
+		// Set calendar
+		$this->table->set_template(array('table_open'  => '<table>'));
     }
 
 	public function events($year = null, $month = null, $day = null, $offset = 0)
@@ -94,8 +94,105 @@ class Em_events extends Public_Controller
 			->build('front/view');
 	}
 	
-	public function calendar()
+	public function calendar($year = null, $month = null)
 	{
+		$this->template->title('Upcoming Events');
 		
+		$params = array(
+			'stream' => 'events',
+			'namespace' => 'events_manager',
+			'order_by' => 'start',
+			'sort' => 'asc',
+			'date_by' => 'start',
+			'show_past' => 'no',
+			'paginate' => 'yes',
+			'pag_segment' => 3,
+			'month' => $month ? $month : date('n'),
+			'year' => $year ? $year : date('Y')
+		);
+		
+		$results = $this->streams->entries->get_entries($params);
+		
+		$events = $results['entries'];
+		
+		// Assign events to keyed array ex. $var[4] = array(event1, event2, etc.)
+		foreach($events as $event)
+		{
+			$event = (object) $event;
+			
+			$event_day = date('j', $event->start);
+
+			$event_days[$event_day][] = array(
+				'title' => $event->title,
+				'url' => site_url('event' . date('/Y/m/d/', $event->start) . $event->slug)
+			);
+		}
+		
+		// Consolidate events into uls and assign to a day
+		if(isset($event_days))
+		{
+			foreach($event_days as $day => $event)
+			{
+				$cell = $this->template
+					->set_layout(null)
+					->set('events', $event_days[$day])
+					->set('day', $day)
+					->build('front/calendar/cell_format', '', true);
+					
+				$event_list[$day] = $cell;
+			}
+		}
+
+		$prev_month = $month == 1 ? 12 : $month - 1;
+		$prev_year = $prev_month == 12 ? $year - 1 : $year;
+		
+		$next_month = $month == 12 ? 1 : $month + 1;
+		$next_year = $next_month == 1 ? $year + 1 : $year;
+		
+		$previous = anchor(site_url('calendar/'.$prev_year.'/'.$prev_month), '&larr;');
+		$next     = anchor(site_url('calendar/'.$next_year.'/'.$next_month), '&rarr;');
+		
+		$prefs['template'] = '
+
+		   {table_open}<table id="calendar">{/table_open}
+
+		   {heading_row_start}<tr id="calendar-heading">{/heading_row_start}
+
+		   {heading_title_cell}<th colspan="{colspan}">'.$previous.' '.anchor(site_url('events/calendar/'.$year.'/'.$month), '{heading}').' '.$next.'</th>{/heading_title_cell}
+
+		   {heading_row_end}</tr>{/heading_row_end}
+
+		   {week_row_start}<tr id="weekdays">{/week_row_start}
+		   {week_day_cell}<td>{week_day}</td>{/week_day_cell}
+		   {week_row_end}</tr>{/week_row_end}
+
+		   {cal_row_start}<tr>{/cal_row_start}
+		   {cal_cell_start}<td>{/cal_cell_start}
+
+		   {cal_cell_content}{content}{/cal_cell_content}
+		   {cal_cell_content_today}{content}{/cal_cell_content_today}
+
+		   {cal_cell_no_content}<div class="wrap no-event"><span class="day-num">{day}</span></div>{/cal_cell_no_content}
+		   {cal_cell_no_content_today}<div class="wrap no-event"><span class="day-num today">{day}</span></div>{/cal_cell_no_content_today}
+
+		   {cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+		   {cal_cell_end}</td>{/cal_cell_end}
+		   {cal_row_end}</tr>{/cal_row_end}
+
+		   {table_close}</table>{/table_close}
+		';
+		
+		$prefs['day_type']     = 'long';
+
+		$this->load->library('calendar', $prefs);
+
+		$data->month = $month;
+		$data->year = $year;
+		$data->event_list = $event_list;
+
+		$this->template
+			->set_layout('default.html')
+			->build('front/calendar/view', $data);
 	}
 }
