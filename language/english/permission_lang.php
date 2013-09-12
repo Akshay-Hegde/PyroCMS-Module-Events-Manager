659f8a76a0b46f8d884cf82ee6cc732c177b80a4
Phil Martinez <phil@philsquare.com>
2013-09-12 16:43:20 -0500

    fixed lang slug so roles are outputted on permission page

--- language/english/permission_lang.php
+++ language/english/permission_lang.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Permissions
-$lang['events_manager:categories'] = 'Categories';
-$lang['events_manager:custom_fields'] = 'Custom Fields';
+$lang['events_manager:role_categories'] = 'Categories';
+$lang['events_manager:role_custom_fields'] = 'Custom Fields';
