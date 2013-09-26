23c9abf99b8146ea41f018f931db7faa6c896fea
Phil Martinez <phil@philsquare.com>
2013-09-26 09:57:09 -0500

    updated help section

--- language/english/help_lang.php
+++ language/english/help_lang.php
@@ -10,4 +10,10 @@ $lang['help_body'] = '
 <li>If you plan on using categories, visit the "Colors" and "Categories" sections and set them up appropriately to meet your needs.</li>
 <li>Under the "Custom Fields" section you can define some addition fields that are specific to your event requirements.</li>
 </ol>
-<p>Please visit the "Support Page" if you run into any issues or would like to submit a feature request. Thanks for using Events Manager by Philsquare Labs</p>';
+<h4>Registrations</h4>
+<p>Registrations can be enabled in the "Settings" section of you admin panel. Once enabled you will be able to optional select your event to except registrations. Please note the the registrations require no account or login by the user. It simply collects a name and email. We plan on extending the functionality of registration in future version of Events Manager.</p>
+<h4>Category Colors</h4>
+<p>Currently, there is no color picker for the colors. You will have to update the colors using the HEX value. You can use other resources such as <a href="http://www.colorpicker.com/" target="_blank">www.colorpicker.com</a> to help get the codes.</p>
+
+<h4>Support</h4>
+<p>Please visit <a href="http://philsquarelabs.com" target="_blank">philsquarelabs.com</a> if you run into any issues or would like to submit a feature request. Thanks for using Events Manager.</p>';
