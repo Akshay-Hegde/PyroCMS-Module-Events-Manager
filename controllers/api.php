73d3d121b502dd2e9d1d3a4382508f05b37d8f1d
Phil Martinez <phil@philsquare.com>
2013-09-26 12:02:48 -0500

    created ajax controller

---
+++ controllers/api.php
@@ -0,0 +1,39 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Events Manager module
+ *
+ *
+ * @author 		Phil Martinez - Philsquare Dev Team
+ * @website		http://philsquare.com
+ * @package 	PyroCMS
+ */
+
+class Api extends Public_Controller
+{
+	public function __construct()
+    {
+        parent::__construct();
+
+		// Only AJAX gets through!
+	   	if( ! $this->input->is_ajax_request() ) die('Invalid request.');
+    }
+	
+	public function get()
+	{
+		$params = array(
+			'stream' => 'events',
+			'namespace' => 'events_manager',
+			'limit' => $this->input->post('limit', true),
+			'offset' => $this->input->post('offset', true),
+			'order_by' => 'start',
+			'sort' => 'asc',
+			'date_by' => 'start',
+			'show_past' => 'no'
+		);
+		
+		$events = $this->streams->entries->get_entries($params);
+		
+		echo json_encode($events);
+	}
+}
