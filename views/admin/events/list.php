96cb76f4bdd89f263b6b6b3719a4438f1ab9a154
Phil Martinez <phil@philsquare.com>
2014-08-12 14:02:21 -0500

    removing CI table lib code

--- views/admin/events/list.php
+++ views/admin/events/list.php
@@ -1,42 +1,38 @@
 <?php if(isset($template['partials']['filters'])) echo $template['partials']['filters'] ?>
 
 <?php if(count($events)): ?>
-
-<?php
-
-	$this->table->set_heading('Date', 'Event', '&nbsp;');
 	
-	foreach($events['entries'] as $event)
-	{
-		$event = (object) $event;
-		
-		// Column Data
-		$column1 = date(Settings::get('date_format'), $event->start);
-		$column2 = $event->title;
-		
-		// Actions
-		$actions = array();
-		$actions[] = ($event->registration['key'] == 'yes') ? anchor(site_url("admin/events_manager/registrations/$event->id"), 'Registrants (' . $event->registration_count . ')', 'class="button"') : '';
-		$actions[] = anchor(site_url('events_manager/event' . date('/Y/m/d/', $event->start) . $event->slug), 'View', 'class="button"');
-		$actions[] = anchor(site_url("admin/events_manager/form/$event->id"), 'Edit', 'class="button"');
-		$actions[] = anchor(site_url("admin/events_manager/delete/$event->id"), 'Delete', 'class="button confirm"');
-		
-		$actions = array(
-			'data' => implode(' ', $actions),
-			'class' => "actions"
-		);
-		
-		$this->table->add_row($column1, $column2, $actions);
-	}
+	<table>
+		<thead>
+			<tr>
+				<th>Date</th>
+				<th>Event</th>
+				<th>&nbsp;</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($events['entries'] as $event): ?>
+				<tr>
+					<td><?php echo date(Settings::get('date_format'), $event['start']) ?></td>
+					<td><?php echo $event['title'] ?></td>
+					<td class="actions">
+						<?php if($event['registration']['key'] == 'yes'): ?>
+							<?php echo anchor(site_url("admin/events_manager/registrations/" . $event['id']), 'Registrants (' . $event['registration_count'] . ')', 'class="button"') ?>
+						<?php endif ?>
+					
+						<?php echo anchor(site_url('events_manager/event' . date('/Y/m/d/', $event['start']) . $event['slug']), 'View', 'class="button"') ?>
+						<?php echo anchor(site_url("admin/events_manager/form/" . $event['id']), 'Edit', 'class="button"') ?>
+						<?php echo anchor(site_url("admin/events_manager/delete/" . $event['id']), 'Delete', 'class="button confirm"') ?>
+					</td>
+				</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
 
-	echo $this->table->generate();
-	
-	echo $pagination;
-
-?>
+	<?php echo $pagination ?>
 
 <?php else: ?>
 
-<div class="no_data">No Events</div>
+	<div class="no_data">No Events</div>
 
 <?php endif ?>
