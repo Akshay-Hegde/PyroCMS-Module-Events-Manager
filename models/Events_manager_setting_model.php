fd626903e0af5325761f769c8d943df7081cb06f
Phil Martinez <phil@philsquare.com>
2014-12-09 21:36:57 -0600

    fixing case issue 1/2

--- models/Events_manager_setting_model.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-class Events_manager_setting_model extends Events_manager_base_model {
-	
-	protected $stream = 'settings';
-	
-}
