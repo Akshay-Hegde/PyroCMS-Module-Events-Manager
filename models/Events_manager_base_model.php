27a17264e9d68e1c48d79186d6febc70367d5291
Phil Martinez <phil@philsquare.com>
2014-12-09 21:38:57 -0600

    ugg.....

--- models/Events_manager_base_model.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-class Events_manager_base_model extends Philsquare_stream_model {
-	
-	protected $namespace = 'philsquare_events_manager';
-	
-	protected $disable = 'id|created|image|updated|created_by';
-	
-}
